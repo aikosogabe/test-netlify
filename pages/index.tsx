79e69d03242ba56a49af2eb3de9dfd086494f80e
aikosogabe <aiko.sogabe@fuller.co.jp>
2021-05-18 17:18:07 +0900

    all files

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>HOME</h1>
+        <Link href="/posts/page1/">
+          <a>page1</a>
+        </Link>
+      </main>
+      <footer className={styles.footer}>フッター</footer>
+    </div>
+  );
+}
