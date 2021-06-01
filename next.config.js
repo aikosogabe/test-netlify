58a2a18ba921bf4cb8b5ea4b70768b2e0ad129d1
aikosogabe <aiko.sogabe@fuller.co.jp>
2021-06-01 16:21:33 +0900

    no message

---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pageExtensions: ["mdx", "tsx"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+    return config;
+  },
+  reactStrictMode: true,
+};
